5b1e29e73f7132d2000d8a4bf1d38020f637e461
thomasxuetao <xuetao78@139.com>
2016-12-01 18:49:01 +0800

    v1.31

--- Test/src/com/example/test/MainActivity.java
+++ Test/src/com/example/test/MainActivity.java
@@ -1,18 +1,47 @@
 package com.example.test;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	private Button actButton;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("MainActivity","onCreate");
+        actButton = (Button)findViewById(R.id.button1);
+        actButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				//1.拨打电话
+				// 给移动客服10086拨打电话
+				Uri uri = Uri.parse("tel:10086");
+				Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+				startActivity(intent);
+				
+			}
+		});
+        
+        new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+			}
+		}).start();
     }
+    
 
 
     @Override
